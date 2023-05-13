ec9bd5778977a6fd5327c37dcaf24d5c19f0f165
Justpie8 <navaltajohan@gmail.com>
2023-05-13 20:36:18 +0545

    added lexer and token
    lexer.go for creating tokens from input
    lexer_test.go for testing lexer.go
    token.go for storing all tokens

--- token/token.go
+++ token/token.go
@@ -1,8 +1,43 @@
 package token
 
+// defining TokenTypes as const
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" //variables or fn names like add,foobar,x,y
+	INT   = "INT"   //number like 1234
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
