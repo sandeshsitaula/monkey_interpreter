b4b04096ad0e5c4195f797e02dcda1b7b8589169
Justpie8 <navaltajohan@gmail.com>
2023-05-15 20:13:45 +0545

    updated to handle integer literal and prefix expression
    includes like : 5,-5,!5

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/sandeshsitaula/monkeyinter/ast"
 	"github.com/sandeshsitaula/monkeyinter/lexer"
@@ -40,6 +41,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.nextToken()
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	return p
 }
 
@@ -109,6 +111,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 }
 
+// for identifier
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 	stmt.Expression = p.parseExpression(LOWEST)
@@ -130,6 +133,23 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+///////////
+
+//for integer literal
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
+
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
