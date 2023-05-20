0daf34f9a511d782a9a012447e50347a6dd02df1
Justpie8 <navaltajohan@gmail.com>
2023-05-20 09:50:18 +0545

    updated repl to display parsed data

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/sandeshsitaula/monkeyinter/lexer"
-	"github.com/sandeshsitaula/monkeyinter/token"
+	"github.com/sandeshsitaula/monkeyinter/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +22,20 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
